52336bc7aa0834fd96a86365b11a5a1caf732005
mnoor01 <MuhaimenNoor@ac.c4q.nyc>
2017-12-12 15:11:11 -0500

    initial commit

---
+++ app/src/main/java/com/example/c4q/hw12googlenow/GoogleAdapter.java
@@ -0,0 +1,25 @@
+package com.example.c4q.hw12googlenow;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by c4q on 12/12/17.
+ */
+
+public class GoogleAdapter extends RecyclerView.Adapter<GoogleViewHolder> {
+    @Override
+    public GoogleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(GoogleViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/c4q/hw12googlenow/GoogleViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.c4q.hw12googlenow;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by c4q on 12/12/17.
+ */
+
+public class GoogleViewHolder extends RecyclerView.ViewHolder {
+    public GoogleViewHolder(View itemView) {
+        super(itemView);
+    }
+}
