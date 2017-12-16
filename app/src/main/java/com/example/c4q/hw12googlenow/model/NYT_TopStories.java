6cea204b9750b62db082c05bf99b9bd41489c828
Bobby Bah <boubacarbah@ac.c4q.nyc>
2017-12-16 16:12:16 -0500

    added data model schema

--- app/src/main/java/com/example/c4q/hw12googlenow/model/NYT_TopStories.java
+++ app/src/main/java/com/example/c4q/hw12googlenow/model/NYT_TopStories.java
@@ -1,8 +1,21 @@
 package com.example.c4q.hw12googlenow.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by bobbybah on 12/13/17.
  */
 
 public class NYT_TopStories {
+    String status;
+    String copyright;
+    String title;
+    String last_updated;
+    List<results> results = new ArrayList<>();
+
+    public List<results> getResults(){
+        return this.results;
+    }
+
 }
--- app/src/main/java/com/example/c4q/hw12googlenow/networking/NYTimesAPI.java
+++ app/src/main/java/com/example/c4q/hw12googlenow/networking/NYTimesAPI.java
@@ -1,5 +1,8 @@
 package com.example.c4q.hw12googlenow.networking;
 
+import com.example.c4q.hw12googlenow.model.NYT_TopStories;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 /**
@@ -11,5 +14,5 @@ public interface NYTimesAPI {
     String NYT_Top_APIKey = "d752126a9dec4034b7f3e0e71ab84825";
 
     @GET("topstories/v2/home.json?" + NYT_Top_APIKey)
-
+    Call<NYT_TopStories> //getter for NYT_TopStories object
 }
