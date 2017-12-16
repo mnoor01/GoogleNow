d6d5b040d7c71e2d9825c995cffbdfccdd725a89
mnoor01 <MuhaimenNoor@ac.c4q.nyc>
2017-12-16 16:16:05 -0500

    added reetrfit

--- app/src/main/java/com/example/c4q/hw12googlenow/BBCViewHolder.java
+++ app/src/main/java/com/example/c4q/hw12googlenow/BBCViewHolder.java
@@ -2,13 +2,22 @@ package com.example.c4q.hw12googlenow;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by c4q on 12/12/17.
  */
 
 public class GoogleViewHolder extends RecyclerView.ViewHolder {
+    private TextView articleList;
     public GoogleViewHolder(View itemView) {
         super(itemView);
+        articleList= (TextView) itemView.findViewById(R.id.listofarticles);
+
+    }
+
+    public void BindingArticlesIGuess(GoogleModel instantiatedModel){
+
+        instantiatedModel.setArticle(articleList.toString());
     }
 }
